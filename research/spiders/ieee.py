d95686f00af2c24d2927e119767ddfb347af8d2b
Helder Willian <helderwmarcos@gmail.com>
2018-03-07 11:30:40 -0300

    Commit 02

--- research/spiders/ieee.py
+++ research/spiders/ieee.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from scrapy.linkextractor import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy_splash import SplashRequest
 
 
 class IeeeSpider(CrawlSpider):
@@ -14,9 +15,19 @@ class IeeeSpider(CrawlSpider):
             LinkExtractor(
                 allow='/conferences_events/conferences/search/index.html'
             ),
-            callback='parse_conference'
+            callback='parse_conference',
+            process_request='splash_request'
         )
     )
+
+    def splash_request(self, request):
+        return SplashRequest(
+            url=request.url,
+            callback=self.parse_conference(),
+            args={
+                'wait': 2,
+            }
+        )
 
     def parse_conference(self, response):
         title = response.xpath('//title/text()').extract_first()
